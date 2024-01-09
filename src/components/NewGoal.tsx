0c7f958d5b851606113fead1e8ffbb2080adb7fb
jayantced739 <109576199+jayantced@users.noreply.github.com>
2024-01-09 22:12:50 +0530

    end

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,20 +1,34 @@
-import { type FormEvent } from "react";
+import { useRef, type FormEvent } from "react";
 
-export default function NewGoal() {
+type NewGoalsProps = {
+  onAddGoal: (goal: string, summary: string) => void
+}
+
+export default function NewGoal({onAddGoal}: NewGoalsProps) {
+
+  const goal = useRef<HTMLInputElement>(null);
+  const summary = useRef<HTMLInputElement>(null);
 
     function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
+
+        const enteredGoal = goal.current!.value;
+        const enteredSummary = summary.current!.value;
+
+        
+        onAddGoal(enteredGoal, enteredSummary);
+        event.currentTarget.reset();
     }
 
   return (
     <form onSubmit={handleSubmit}>
       <p>
         <label htmlFor="goal">Your Goal</label>
-        <input id="goal" type="text" />
+        <input id="goal" type="text" ref={goal} />
       </p>
       <p>
         <label htmlFor="summary">Your Goal</label>
-        <input id="summary" type="text" />
+        <input id="summary" type="text" ref={summary} />
       </p>
       <p>
         <button>Add Goal</button>
